1fb12cf2dd34e9fff03e59434a67141b0d4f4d7b
rajat-sharma27 <rajatpndt2703@gmail.com>
2025-11-17 10:38:10 +0530

    Lectures

---
+++ LEC11/array.js
@@ -0,0 +1,21 @@
+// let arr = [1,2,3,4,5];
+
+// let mapArr = arr.map((x) => x*2);
+
+// arr.filter((x) => x%2 == 0);
+
+// arr.reduce((a,b) => a+b);
+// console.log(mapArr);
+// arr.shift();
+
+// arr.unshift(7);
+
+// counters
+
+// console.log("10"+5)
+// console.log(5+"10")
+// console.log(true+5)
+// console.log(null+5)
+// console.log(undefined+5)
+
+// Errors
